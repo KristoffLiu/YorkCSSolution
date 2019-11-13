b31101cdc64d830f33478917832f66ddcf420f33
Zhikang Liu <zl1748@its.york.ac.uk>
2019-11-13 11:56:36 +0000

    Update 2

--- Software 1/Week 07/Practical 11/recursion.py
+++ Software 1/Week 07/Practical 11/recursion.py
@@ -23,5 +23,47 @@ def rec_sum(numbers):
 
 #Exercise 04
 def iselfish(word):
-    if word[0:2].isnumeric():
-        
+    if word[0:3].isnumeric():
+        if word[3] == "e":
+            word[0] == "1"
+        elif word[3] == "l":
+            word[1] == "1"
+        elif word[3] == "f":
+            word[2] == "1"
+        if word[0:3] == "111":
+            return True
+        elif len(word) > 4:
+            return False
+        else:
+            return iselfish(word[0:3] + word[4:len(word)])
+    else:
+        return iselfish("000" + word)
+
+def something_ish(pattern,word):
+    if pattern:
+        if word.count(pattern[0]) > 0:
+            return something_ish(pattern[1:len(pattern)],word)
+        else:
+            False
+    else:
+        return True
+
+#Exercise 05
+def flatten(mlist):
+    if mlist:
+        if type(mlist[0]) == list:
+            return flatten(mlist.pop(0)) + flatten(mlist)
+        else:
+            return mlist.pop(0) + flatten(mlist)
+    else:
+        return 0
+
+#Exercise 06
+def merge(sorted_listA,sorted_listB):
+    if sorted_listA and sorted_listB:
+        a = sorted_listA.pop(0)
+        b = sorted_listB.pop(0)
+        sortedpart = [a] + [b] if sorted_listA[0] > sorted_listB[0] else [b] + [a]
+        return sortedpart + merge(sorted_listA,sorted_listB)
+    else:
+        return []
